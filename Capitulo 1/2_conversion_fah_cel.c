b24635858f85e50d749a4c650850833c386d991e
Mateo <mateoghiano10@hotmail.com>
2023-01-23 15:13:24 -0300

    Conversor finished

--- Capitulo 1/2_conversion_fah_cel.c
+++ Capitulo 1/2_conversion_fah_cel.c
@@ -16,6 +16,11 @@ main(){
     upper=300;
     step=20;
 
-    printf("%d\n", upper);
+    fahr = lower;
 
+    while(fahr<=upper){
+        celsius = 5*(fahr-32)/9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;  
+    }
 }   
---
+++ Capitulo 1/cap1_pr02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main(){
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = -10;
+    upper = 50;
+    step = 10;
+
+    celsius = lower;
+    while (celsius <= upper)
+    {
+        fahr = ((celsius * 9)/5) + 32;
+        printf("%d\t%d\n", celsius, fahr);
+        celsius += step;
+    }
+    
+}
