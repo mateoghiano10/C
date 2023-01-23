622ca272ed7a2a5d1f3c72a0a444b856664fc31d
Mateo <mateoghiano10@hotmail.com>
2023-01-23 01:03:18 -0300

    First two prgrams

---
+++ Capitulo 1/2_conversion_fah_cel.c
@@ -0,0 +1,21 @@
+/*
+    CURSO C LIBRO KERNIGHAN & RITCHIE
+    programa de conversion de Fahrenhein a Celsius
+
+    capitulo1, variables, expresiones e iteraciones
+*/
+
+#include <stdio.h>
+// imprime la tabla de F-C para fahr=0,20,...,300
+
+main(){
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower=0;
+    upper=300;
+    step=20;
+
+    printf("%d\n", upper);
+
+}   
---
+++ Capitulo 1/cap1_pr01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main(){
+    printf("Mateo\n");
+    printf("Como estas?\n");
+}
