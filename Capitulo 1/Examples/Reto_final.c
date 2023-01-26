c1302e0a51849eb61752c9889a900d2fef75870c
Mateo <mateoghiano10@hotmail.com>
2023-01-26 10:21:47 -0300

    Chapter one finished

--- Capitulo 1/Examples/Reto_final.c
+++ Capitulo 1/Examples/Reto_final.c
@@ -10,20 +10,34 @@
 
 
 main(){
-    int n, state, gu, gc;
+    int n, state, gu, gc; // user guess, computer guess, number
     state = OUT;
+    srand(getpid()); 
     n = rand () % (122-97+1) + 97; 
+    printf("Bienvenido a take a guess!\nEste juego consiste en " 
+        "adivinar una letra antes que la maquina lo haga por ti.\n");
     while (state != IN)
-    {
+    {                    
         gu = getchar();
-        if (gu==n)
+        if (gu != n)
         {
+            printf("Lo siento! Esa no era la letra.\n");
+            if (gu > n)
+                printf("Debe de ingresar una letra menor a la ingresada.\n");
+            else if (gu < n)
+                printf("Debe de ingresar una letra mayor a la ingresada.\n");
+        }
+        else if (gu == n)
+        {
+            printf("Felicidades! Acertaste!\n");
             state = IN;
         }
-        else if ((gc = rand () % (122-97+1) + 97) == n)
+        if ((gc =rand () % (122-97+1) + 97) == n)
+        {
+            printf("La maquina ha acertado el numero.\n");
             state = IN;
-        
+        }
+        gu = getchar();    
     }
-    
-    
+    printf("Gracias por participar!");
 }
